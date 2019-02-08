31fb7accfb77c36188537710b0a50d08d5542201
Romain HALLAY <romain.hallay2016@campus-eni.fr>
2019-02-08 11:22:47 +0100

    modif error connectionstring

--- TestAuthentification/Startup.cs
+++ TestAuthentification/Startup.cs
@@ -30,7 +30,7 @@ namespace TestAuthentification
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
