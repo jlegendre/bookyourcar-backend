6e08c4ed7cf6b8dcdd9a4c373de8a0413ef09aff
jeremy <jeremy.legendre@free.fr>
2019-05-10 10:02:48 +0200

    ajout required sur location

--- TestAuthentification/ViewModels/Location/LocationViewModel.cs
+++ TestAuthentification/ViewModels/Location/LocationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TestAuthentification.ViewModels.Location
 {
@@ -10,23 +11,28 @@ namespace TestAuthentification.ViewModels.Location
         /// <summary>
         /// date de début d'une reservation
         /// </summary>
+        [Required]
         public DateTime DateDebutResa { get; set; }
         /// <summary>
         /// date de fin d'une reservation
         /// </summary>
+        [Required]
         public DateTime DateFinResa { get; set; }
 
         /// <summary>
         /// id du pôle de prise charge du véhicule
         /// </summary>
+        [Required]
         public int PoleIdDepart { get; set; }
         /// <summary>
         /// id du pôle de retour du véhicule
         /// </summary>
+        [Required]
         public int PoleIdDestination { get; set; }
         /// <summary>
         /// commentaire de la demande de location
         /// </summary>
+        [Required]
         public string Comments { get; set; }
 
         
